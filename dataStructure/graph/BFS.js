a68cb9fefd19d6eff66830d129a7fe99612e6af9
sean <tmddnr3163@gmail.com>
2021-07-27 21:58:39 +0900

    BFS & DFS

---
+++ dataStructure/graph/BFS.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+
+class StationNode {
+  constructor(stationName) {
+    this.stationName = stationName;
+    this.adjacentStations = [];
+  }
+
+  addConnection(otherStation) {
+    this.adjacentStations.push(otherStation);
+    otherStation.adjacentStations.push(this);
+  }
+
+  log() {
+    let str = `${this.stationName}`;
+
+    for (let station of this.adjacentStations) {
+      str += `${station.stationName}`;
+    }
+
+    return str;
+  }
+}
+
+function createStationNodes(inputFile) {
+  const stations = {};
+
+  return new Promise((reslove, reject) => {
+    fs.readFile(inputFile, 'utf8', (err, data) => {
+      for (let line of data.split('\n')) {
+        let previousStation = null;
+        let currentStation = null;
+        const subwayLine = line.trim().split(' - ');
+
+        for (let name of subwayLine) {
+          const stationName = name.trim();
+
+          if (!stations.hasOwnProperty(stationName)) {
+            currentStation = new StationNode(stationName);
+            stations[stationName] = currentStation;
+          } else {
+            currentStation = stations[stationName];
+          }
+
+          if (previousStation !== null) {
+            currentStation.addConnection(previousStation);
+          }
+
+          previousStation = currentStation;
+        }
+      }
+      reslove(stations);
+    });
+  });
+}
+
+function bfs(graph, startNode) {
+  const queue = [];
+
+  for (let stationNode of Object.values(graph)) {
+    stationNode.visited = false;
+  }
+
+  startNode.visited = true;
+  queue.push(startNode);
+
+  while (queue.length > 0) {
+    const currentStation = queue.shift();
+
+    for (station of currentStation.adjacentStations) {
+      if (station.visited === false) {
+        station.visited = true;
+        queue.push(station);
+      }
+    }
+  }
+}
+
+createStationNodes('./new_stations.txt').then((stations) => {
+  const gangNamStation = stations['강남'];
+  bfs(stations, gangNamStation);
+  console.log(stations['강동구청'].visited);
+  console.log(stations['평촌'].visited);
+  console.log(stations['송도'].visited);
+  console.log(stations['개화산'].visited);
+  console.log(stations['반석'].visited);
+  console.log(stations['지족'].visited);
+  console.log(stations['노은'].visited);
+  console.log(stations['(대전)신흥'].visited);
+});
